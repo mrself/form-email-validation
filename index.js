7c2271c81d5c17b9e0bed1f7500fd40eb173ccca
Lightman <yaruk.my@gmail.com>
2016-10-13 12:10:18 +0300

    refactor: use binding for callback

--- index.js
+++ index.js
@@ -120,9 +120,7 @@ EmailValiditon.prototype = {
 		this.validate(this.value).done(function(result) {
 			EmailValiditon.setCache(self.value, result);
 			self.setState(result);
-		}).fail(function() {
-			self.setState(EmailValiditon.STATES.UNDEFINED);
-		});
+		}).fail(this.setState.bind(this, EmailValiditon.STATES.UNDEFINED));
 	},
 
 	/**
